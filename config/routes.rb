99f3bd0d564a65fe4128086ed95ccf8d513d4517
Jason Barry <jbarry@hey.com>
2023-11-16 17:14:46 -0500

    Allow for deleting cover images

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.htmlz
+
   devise_for :users
   
-  resources :blog_posts
+  resources :blog_posts do
+    resource :cover_image, only: [:destroy], module: :blog_posts
+  end
 
   # Defines the root path route ("/")
   root "blog_posts#index"
